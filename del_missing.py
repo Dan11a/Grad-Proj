78dae76c5413983dc0a7fd4a192f576e23406f4d
dan11a <dania_hamed@outlook.com>
2025-03-05 15:44:15 +0300

    Initial commit: Project setup

---
+++ del_missing.py
@@ -0,0 +1,15 @@
+import json
+
+file_path = 'archive/WLASL_v0.3.json'
+
+with open(file_path, 'r') as json_file:
+    metadata = json.load(json_file)
+
+    for ent in metadata:
+        ent['instances'] = [ inst for inst in ent['instances'] if 'video_path' in inst ]
+
+with open (file_path, 'w') as json_file:
+    json.dump(metadata, json_file, indent=4)
+
+print("file updated successfully.")
+
